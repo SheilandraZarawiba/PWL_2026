c2b57a220851091fc90c0d067308ff1caba73281
sheiyyara <sheilandrazarawiba561@gmail.com>
2026-02-27 21:37:06 +0700

    Implement WelcomeController, PageController, and resource controller

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        return 'Nama: Sheilandra Zarawiba, NIM: 244107020110';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -41,3 +48,23 @@ Route::get('/user/{name?}', function ($name=null) {
 Route::get('/user/{name?}', function ($name='John') {
  return 'Nama saya '.$name;
 });
+
+// membuat route dengan controller
+Route::get('/hello', [WelcomeController::class, 'hello']);
+Route::get('/', [PageController::class, 'index']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/articles/{id}', [PageController::class, 'articles']);
+
+// membuat route dengan single action controller
+Route::get('/', HomeController::class);
+Route::get('/about', AboutController::class);
+Route::get('/articles/{id}', ArticleController::class);
+
+// membuat route dengan resource controller
+Route::resource('photos', PhotoController::class);
+Route::resource('photos', PhotoController::class)->only([
+ 'index', 'show'
+]);
+Route::resource('photos', PhotoController::class)->except([
+ 'create', 'store', 'update', 'destroy'
+]);
